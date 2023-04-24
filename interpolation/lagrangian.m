c6c9a1a3db99602bd0fd313c71931428d15b126e
Harshit Goyal <97736991+Harshit0143@users.noreply.github.com>
2023-04-24 22:35:40 +0530

    Create lagrangian.m

---
+++ interpolation/lagrangian.m
@@ -0,0 +1,22 @@
+x = [1 1.3 1.6 1.9 2.2];
+y = [ 0.1411 -0.6878 -0.9962 -0.5507 0.3115];
+
+arr = 0:0.0001:100;
+y = arrayfun(@lagrange,arr);
+plot(arr,y,'.')
+
+function z = lagrange(k)
+    x = [1 1.3 1.6 1.9 2.2];
+    y = [ 0.1411 -0.6878 -0.9962 -0.5507 0.3115];
+    n = length(x);
+    z = 0;
+    for i = 1 : n
+        p = y(i);
+        for j = 1:n
+            if j~=i
+                p = p*(k-x(j))/(x(i)-x(j));
+            end
+        end
+        z = z + p;
+    end
+end
