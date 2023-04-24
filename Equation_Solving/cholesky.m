94ee4f5630e1924833cfa58b3c18ddd48299d636
Harshit Goyal <97736991+Harshit0143@users.noreply.github.com>
2023-04-24 19:56:42 +0530

    Update cholesky.m

--- Equation_Solving/cholesky.m
+++ Equation_Solving/cholesky.m
@@ -1,20 +1,49 @@
-A = [ 6 15 55;
-      15 55 225;
-      55 225 979];
-disp(A);
-A = cholesky_(A);
-disp(A);
-function A = cholesky_(A)
-    n = length(A);
-    for k = 1: n
+format long
+n  = 5;
+n = 5;
+R = randn(n, n);
+A = R * R';
+while ~all(eig(A) >= 0)
+    R = randn(n, n);
+    A = R * R';
+end
+
+b = randn(n,1);
+
+x = cholesky(A,b);
+fprintf("Solution by cholesky Decomposition: x =\n");
+disp(x);
+x_in = A\b;
+
+fprintf("Solutin by MATLAB in built A\b: x_inbuilt =\n");
+disp(x_in);
+
+
+function x = cholesky(A,b)
+   n = length(A);
+    for k = 1:n
         for i = 1:k-1
-            A(k,i) = (A(k,i) - dot(A(k,1:i-1),A(i,1:i-1)))/A(i,i);
+            A(k,i) = (A(k,i) - dot(A(k,1:i-1),A(i,1:i-1)) ) /A(i,i);
         end
-    A(k,k) = sqrt( A(k,k)  - sum(A(k, 1:k-1).^2) );     
+        A(k,k) = sqrt( A(k,k)  - dot(A(k, 1:k-1),A(k,1:k-1)) );     
     end
+    disp(A);
+    % Ly = b;
+    % Ux = y
     for i = 1:n
-        for j = i+1 :n
+        for j = i+1: n
             A(i,j) = A(j,i);
         end
+
     end
-end
+    y = zeros(n,1);
+    y(1) = b(1)/A(1,1);
+    for i = 2 : n
+        y(i) = (b(i) - A(i,1:i-1)*y(1:i-1))/A(i,i);
+    end
+    x = zeros(n,1);
+    x(n) = y(n)/A(n,n);
+    for i = n-1 : -1 : 1
+        x(i) = (y(i) - A(i,i+1:n)*x(i+1:n))/A(i,i);
+    end
+end 
